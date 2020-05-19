88ff56322fdc5add73e5eb627de9fd0db97581d3
giadarol <giovanni.iadarola@cern.ch>
2020-05-19 16:13:28 +0200

    Implemented y plane in 000

--- 000_response_to_single_sinusoid/000_response_to_sin.py
+++ 000_response_to_single_sinusoid/000_response_to_sin.py
@@ -15,6 +15,8 @@ N_oscillations = 3.00000000e+00
 flag_no_bunch_charge = False
 flag_plots = True
 
+plane = 'y'
+
 sim_param_file = '../reference_simulation/Simulation_parameters.py'
 sim_param_amend_files = [
         '../Simulation_parameters_amend.py',
@@ -49,6 +51,8 @@ for ss in slices:
     if ss.macroparticlenumber:
         ss.x -= ss.mean_x()
         ss.xp -= ss.mean_xp()
+        ss.y -= ss.mean_y()
+        ss.yp -= ss.mean_yp()
 
 # Optionally remove charge from bunch
 if flag_no_bunch_charge:
@@ -63,23 +67,32 @@ z_step = z_slices[1] - z_slices[0]
 z_range = z_slices[-1] - z_slices[0] + z_step # Last term is to make the sampled 
                                               # sinusoids more orthogonal
 # Generate ideal sinusoidal distortion 
-x_ideal = (sin_amplitude * np.sin(2*np.pi*N_oscillations*z_slices/z_range)
+r_ideal = (sin_amplitude * np.sin(2*np.pi*N_oscillations*z_slices/z_range)
          + cos_amplitude * np.cos(2*np.pi*N_oscillations*z_slices/z_range))
 
 # Add sinusoidal distortion to particles
 for ss in slices:
     if ss.macroparticlenumber>0:
         #if ss.mean_z() < 0:
-        ss.x += sin_amplitude * np.sin(2*np.pi*N_oscillations*ss.z/z_range)
-        ss.x += cos_amplitude * np.cos(2*np.pi*N_oscillations*ss.z/z_range)
+        if plane == 'x':
+            ss.x += sin_amplitude * np.sin(2*np.pi*N_oscillations*ss.z/z_range)
+            ss.x += cos_amplitude * np.cos(2*np.pi*N_oscillations*ss.z/z_range)
+        elif plane == 'y':
+            ss.y += sin_amplitude * np.sin(2*np.pi*N_oscillations*ss.z/z_range)
+            ss.y += cos_amplitude * np.cos(2*np.pi*N_oscillations*ss.z/z_range)
+        else:
+            raise ValueError('What?!')
 
 # Measure
-x_slices = np.array([ss.mean_x() for ss in slices])
+if plane == 'x':
+    r_slices = np.array([ss.mean_x() for ss in slices])
+elif plane == 'y':
+    r_slices = np.array([ss.mean_y() for ss in slices])
 int_slices = np.array([ss.intensity for ss in slices])
 
 # Simulate e-cloud interactions
 t_start = time.mktime(time.localtime())
-dpx_slices = []
+dpr_slices = []
 rho_slices = []
 for i_ss, ss in enumerate(slices[::-1]):
     if np.mod(i_ss, 20)==0:
@@ -90,29 +103,39 @@ for i_ss, ss in enumerate(slices[::-1]):
             temp_rho = ee.cloudsim.cloud_list[0].rho.copy()
         else:
             temp_rho += ee.cloudsim.cloud_list[0].rho.copy()
-    dpx_slices.append(ss.mean_xp())
+    if plane == 'x':
+        dpr_slices.append(ss.mean_xp())
+    elif plane == 'y':
+        dpr_slices.append(ss.mean_yp())
     rho_slices.append(temp_rho)
-dpx_slices = np.array(dpx_slices[::-1])
+dpr_slices = np.array(dpr_slices[::-1])
 rho_slices = np.array(rho_slices[::-1])
 t_end = time.mktime(time.localtime())
 print(('Ecloud sim time %.2f s' % (t_end - t_start)))
 
-dpx_slices_all_clouds = dpx_slices * sim_content.n_segments
+dpr_slices_all_clouds = dpr_slices * sim_content.n_segments
 
 # Savings and plots
 first_ecloud = sim_content.parent_eclouds[0]
 xg = first_ecloud.cloudsim.spacech_ele.xg
 yg = first_ecloud.cloudsim.spacech_ele.yg
 
-i_yzero = np.argmin(np.abs(yg))
-rho_cut = rho_slices[:, :, i_yzero]
+if plane == 'x':
+    i_yzero = np.argmin(np.abs(yg))
+    rho_cut = rho_slices[:, :, i_yzero]
+    rg = xg
+elif plane == 'y':
+    i_xzero = np.argmin(np.abs(xg))
+    rho_cut = rho_slices[:, i_xzero, :]
+    rg = yg
 
 
 sio.savemat('response.mat',{
+    'plane': plane,
     'z_slices': z_slices,
-    'x_slices': x_slices,
-    'x_ideal': x_ideal,
-    'dpx_slices_all_clouds': dpx_slices_all_clouds,
+    'r_slices': r_slices,
+    'r_ideal': r_ideal,
+    'dpr_slices_all_clouds': dpr_slices_all_clouds,
     'xg': xg,
     'yg': yg,
     'rho_cut': rho_cut,
@@ -127,8 +150,8 @@ if flag_plots:
     ax3 = fig1.add_subplot(3,1,3, sharex=ax1)
 
     ax1.plot(z_slices, int_slices)
-    ax2.plot(z_slices, x_slices)
-    ax3.plot(z_slices, dpx_slices)
+    ax2.plot(z_slices, r_slices)
+    ax3.plot(z_slices, dpr_slices)
 
     for ax in [ax1, ax2, ax3]:
         ax.grid(True)
@@ -138,10 +161,10 @@ if flag_plots:
     ax21 = fig2.add_subplot(2,1,1)
     ax22 = fig2.add_subplot(2,1,2, sharex=ax21)
 
-    ax21.pcolormesh(z_slices, xg, rho_slices[:, :, i_yzero].T)
-    ax21.plot(z_slices, x_slices, 'k', lw=2)
-    ax22.plot(z_slices, dpx_slices)
-    ax22.set_ylim(np.nanmax(np.abs(dpx_slices))*np.array([-1, 1]))
+    ax21.pcolormesh(z_slices, rg, rho_cut.T)
+    ax21.plot(z_slices, r_slices, 'k', lw=2)
+    ax22.plot(z_slices, dpr_slices)
+    ax22.set_ylim(np.nanmax(np.abs(dpr_slices))*np.array([-1, 1]))
     ax22.grid(True)
     plt.show()
 
